d4fb69dd13aa490047b6286d0942b149c367ac54
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-04 19:33:36 -0300

    Requisito 1 concluído

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+    counter = 0
+    for periods in permanence_period:
+        (entry_time, exit_time) = periods
+        if not isinstance(entry_time, int) or not isinstance(exit_time, int):
+            return None
+        if entry_time <= target_time and exit_time >= target_time:
+            counter += 1
+    return counter
