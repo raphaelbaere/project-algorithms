7ca10d21c5e2eba87cf5c889b242da067d1f191d
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-04 19:52:41 -0300

    lint fix

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,10 +3,10 @@ import pytest
 
 
 def test_encrypt_message():
-    with pytest.raises(TypeError, match=f"tipo inválido para key"):
+    with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message('1', '1')
-    with pytest.raises(TypeError, match=f"tipo inválido para message"):
-            encrypt_message(1, 1)
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 1)
     assert encrypt_message('mensagem', 9) == 'megasnem'
     assert encrypt_message('mensagem', 3) == 'nem_megas'
     assert encrypt_message('mensagem', 2) == 'megasn_em'
