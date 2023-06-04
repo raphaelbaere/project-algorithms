9ace415cbb0c2da66a2f13a476f37b56fd2901a3
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-04 19:49:19 -0300

    Requisito 2 concluído

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match=f"tipo inválido para key"):
+        encrypt_message('1', '1')
+    with pytest.raises(TypeError, match=f"tipo inválido para message"):
+            encrypt_message(1, 1)
+    assert encrypt_message('mensagem', 9) == 'megasnem'
+    assert encrypt_message('mensagem', 3) == 'nem_megas'
+    assert encrypt_message('mensagem', 2) == 'megasn_em'
