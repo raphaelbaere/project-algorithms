d90580f71c4a003f50d4ac1f9cc6dd1387d454f4
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-05 16:10:01 -0300

    Requisito 5 concluído com sucesso

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,24 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1:
+        return False
+    for i in range(1, len(nums)):
+        key = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] > key:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = key
+
+    currentNumber = nums[0]
+    for i in range(1, len(nums)):
+        if isinstance(nums[i], str) or nums[i] < 0 or not nums[i]:
+            return False
+        elif nums[i] == currentNumber:
+            return currentNumber
+        currentNumber = nums[i]
+
+        if i == len(nums) - 1:
+            return False
+        
+
+print(find_duplicate([1, 2, 2, 3, 4]))
