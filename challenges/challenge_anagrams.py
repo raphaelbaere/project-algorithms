d92287086dffd71043022aae336befc20bd061cf
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-05 12:08:58 -0300

    Requisito 4 concluído com sucesso

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,30 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not first_string and not second_string:
+      return (first_string, second_string, False)
+    def partition(arr, low, high):
+      i = low - 1
+      pivot = arr[high]
+
+      for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+      arr[i + 1], arr[high] = arr[high], arr[i + 1]
+      return i + 1
+
+    def quicksort_string(arr, low, high):
+      if low < high:
+        pivot_index = partition(arr, low, high)
+
+        quicksort_string(arr, low, pivot_index - 1)
+        quicksort_string(arr, pivot_index + 1, high)
+
+    def quicksort_string_wrapper(string):
+       arr = list(string.lower())
+       quicksort_string(arr, 0, len(arr) - 1)
+       return ''.join(arr)
+    first_string_ordered = quicksort_string_wrapper(first_string)
+    second_string_ordered = quicksort_string_wrapper(second_string)
+    is_it_anagram = first_string_ordered == second_string_ordered
+    return (first_string_ordered, second_string_ordered, is_it_anagram)
