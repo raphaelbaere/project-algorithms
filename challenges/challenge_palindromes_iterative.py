0a39fbf85d91cbb4bb2461d9f1d64ccd0fba9d80
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-05 16:50:41 -0300

    Requisito 6 concluído

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+      return False
+    is_palindrome = True
+    for i in range(len(word)):
+        highIndex = len(word) - 1 - i
+        if i == highIndex:
+            return True
+        if word[i] != word[highIndex]:
+            is_palindrome = False
+    return is_palindrome
